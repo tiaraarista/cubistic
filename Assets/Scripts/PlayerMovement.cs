d9cd2026de811525849712e6f2971f0aff6f4c0a
tiaraarista <tiaraarista99@gmail.com>
2020-03-30 11:10:01 +0700

    First Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public Rigidbody rb;
+	public float forwardForce = 2000f;
+	public float sidewaysForce = 500f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.AddForce (0, 0, forwardForce * Time.deltaTime);
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rb.AddForce (sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rb.AddForce (-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+	}
+}
