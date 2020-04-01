becba50c054ed9530790d5eb3430af43862db4ba
tiaraarista <tiaraarista99@gmail.com>
2020-04-01 08:03:24 +0700

    Winning Level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,12 @@ public class GameManager : MonoBehaviour {
 	bool gameHasEnded = false;
 	public float restartDelay = 2f;
 
+	public GameObject completeLevelUI;
+
+	public void CompleteLevel(){
+		completeLevelUI.SetActive (true);
+	}
+
 	public void EndGame () {
 		if (gameHasEnded == false) {
 			gameHasEnded = true;
